8e019baa5c1fbdeda1a6b1031d4961be330a4022
Eli Bendersky <eliben@gmail.com>
2023-06-28 16:18:04 -0700

    Use package stuff in main.go

--- main.go
+++ main.go
@@ -9,4 +9,8 @@ import (
 func main() {
 	fmt.Println("Hello from sample-go-program")
 	fmt.Println(quote.Go())
+
+	username := "joemcj"
+	fmt.Println("hash =", hash(username))
+	fmt.Println("is authorized =", isAuthorized(username))
 }
